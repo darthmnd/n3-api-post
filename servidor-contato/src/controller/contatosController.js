29cc1972a9c8922382b7ece6a5ce1d8dfbc79742
darthmnd <andr.mandy@gmail.com>
2019-11-06 22:33:49 -0300

    concatenação dataNasc & nome

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -5,13 +5,27 @@ const getAll = (request, response) => {
   response.status(200).send(model.agenda);
 };
 
-const add = (request, response) => {
-  model.agenda.contatos.push(request.body)
+const comparaDados = (request, response) => {
+  const contato = resquest.body
+  const usuarios = request.body.nome
+  const data = request.body.dataNascimento
+  const concat = usuarios + data
+  // console.log(concat)
+  // if(nomeUsuario && data === add){
+  //   res.redirect(409, "https://http.cat/409") 
+ model.agenda.contatos.push(contato)
   response.status(200).send()
+}
+
+const add = (request, response) => {
+  let dadosContato = request.body
+  dadosContato.id = Math.random().toString(36).substr(-8)
+  model.agenda.contatos.push(dadosContato)
+  response.status(200).send() 
 }
 
 module.exports = {
   getAll,
-  add
+  add,
+  comparaUsuarios: comparaDados
 }
-
--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -5,5 +5,6 @@ const controller = require("../controller/contatosController")
 
 router.get("/", controller.getAll)
 router.post("/criar", bodyParser.json(), controller.add)
+router.get("/concat", controller.comparaUsuarios)
 
 module.exports = router
